910ba705bd0f6f75ec21ef7b3f1512a4bad300f6
zsf90 <zsf.cn.90@gmail.com>
2022-04-03 22:27:00 +0800

    Flash 更新

--- Code/GD32F130F8P6/Src/flash.c
+++ Code/GD32F130F8P6/Src/flash.c
@@ -12,7 +12,7 @@
 
 #define FMC_PAGE_SIZE           ((uint16_t)0x400)   /* 1024 1KB */
 #define FMC_WRITE_START_ADDR    ((uint32_t)0x0800FC00U)
-#define FMC_WRITE_END_ADDR      ((uint32_t)0x0800FFFFU) /* 64KB */
+#define FMC_WRITE_END_ADDR      ((uint32_t)0x08010000U) /* 64KB */
 
 
 /* 定义与初始化 param_t */
@@ -27,11 +27,20 @@ param_t system_param = {
     .time_timer_num     = 60 * 3, /* 3 分钟*/
 };
 
+/**
+ * @brief 写 param 参数到 Flash
+ * 
+ */
 void param_write(void)
 {
 
 }
 
+/**
+ * @brief 读取配置参数到 param 结构体
+ * 
+ * @param param 
+ */
 void param_read(param_t *param)
 {
     static uint32_t i = 0;
@@ -44,4 +53,10 @@ void param_read(param_t *param)
     param->time_week = FMC_READ(FMC_WRITE_START_ADDR+i);
     ++i;
     param->time_hour = FMC_READ(FMC_WRITE_START_ADDR+i);
+    ++i;
+    param->time_minutes = FMC_READ(FMC_WRITE_START_ADDR+i);
+    ++i;
+    param->time_count = FMC_READ(FMC_WRITE_START_ADDR+i);
+    ++i;
+    param->time_timer_num = FMC_READ(FMC_WRITE_START_ADDR+i);
 }
